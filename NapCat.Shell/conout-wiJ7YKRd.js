8ce01a9b8a9215aa5e88dd1c30ec4a384c2b5fff
nico <wjy20250413@163.com>
2026-07-21 15:37:35 +0800

    清理敏感文件和二进制文件，更新 .gitignore

--- NapCat.Shell/conout-wiJ7YKRd.js
+++
@@ -1,8 +0,0 @@
-var e = /* @__PURE__ */ ((r) => (r[r.READY = 1] = "READY", r))(e || {});
-function t(r) {
-  return `${r}-worker`;
-}
-export {
-  e as C,
-  t as g
-};
--- NapCat.Shell/static/assets/use-dialog-vw3O4HMu.js
+++
@@ -1,1 +0,0 @@
-import{R as o}from"./codemirror-core-vkcnXJgD.js";import{a1 as t}from"./index-BfMm4PRv.js";const s=()=>o.useContext(t);export{s as u};
